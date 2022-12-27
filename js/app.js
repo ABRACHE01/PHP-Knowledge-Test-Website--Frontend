3e0491601b294623d088ef6fc0fa9d07bb1f9a2b
medabra <abrache2001@gmail.com>
2022-12-27 22:24:48 +0100

    addig and adjusting js

--- js/app.js
+++ js/app.js
@@ -5,232 +5,219 @@ const answerIndecatorContainer = document.querySelector(".answers-indicator");
 const quizBox = document.querySelector(".quiz-Box");
 const resultBox = document.querySelector(".result-Box");
 
-let questionCounter=0;
+let questionCounter = 0;
 let currentQuestion;
-let availableQestions =[];
-let availableOptions =[];
-let correntAnswers =0;
-let attempt =0;
+let availableQestions = [];
+let availableOptions = [];
+let correntAnswers = 0;
+let attempt = 0;
+let wrongAnswers = 0;
+let timer;
 
-//set quistions available in the data app 
-function setAvailableQuestions(){
 
-    const totalQuestion = quiz.length;
+//set quistions available in the data app
+function setAvailableQuestions() {
+  const totalQuestion = quiz.length;
 
-    for(let i=0;i<totalQuestion;i++){
-        availableQestions.push(quiz[i])
-    }
-    
+  for (let i = 0; i < totalQuestion; i++) {
+    availableQestions.push(quiz[i]);
+  }
 }
 
 
-function  getNewQuestion(){
 
-   //counter of quistiond 
-    questionNumber.innerHTML = "Question "+ (questionCounter+1)+" of "+ quiz.length;
+function getNewQuestion() {
+  //counter of quistiond
+  questionNumber.innerHTML =
+    "Question " + (questionCounter + 1) + " of " + quiz.length;
+  const questionIndex =
+    availableQestions[Math.floor(Math.random() * availableQestions.length)];
 
+  currentQuestion = questionIndex;
+  // console.log(currentQuestion);
+  questionText.innerHTML = currentQuestion.q;
+  const index1 = availableQestions.indexOf(questionIndex);
+  // console.log(index1);
+  availableQestions.splice(index1, 1);
 
-    const questionIndex= availableQestions[Math.floor(Math.random()*availableQestions.length)];
+  //get the length of options
+  const optionLen = currentQuestion.option.length;
+  for (let i = 0; i < optionLen; i++) {
+    availableOptions.push(i);
+  }
 
-     currentQuestion=questionIndex;
-    // console.log(currentQuestion);
-    questionText.innerHTML = currentQuestion.q;
-    const index1= availableQestions.indexOf(questionIndex);
-    // console.log(index1);
-    availableQestions.splice(index1,1);
+  // console.log(availableOptions)
+  document.querySelector(".option-container").innerHTML = "";
 
-    
-
-    //get the length of options
-    const optionLen = currentQuestion.option.length;
-    for( let i=0; i<optionLen ;i++){
-
-        availableOptions.push(i)
-    }
-
-    
-    // console.log(availableOptions)
-    document.querySelector(".option-container").innerHTML="";
-
-    for( let i=0; i< optionLen ; i++){
-        
-      const option = document.createElement("button");
-      option.innerHTML = currentQuestion.option[i];
-      option.id= i;
-      option.className="carts";
-      optionContainer.appendChild(option);
-      option.setAttribute("type","button");
-      option.setAttribute("onclick","getResult(this)"); 
-    }
-    questionCounter++
-   
+  for (let i = 0; i < optionLen; i++) {
+    const option = document.createElement("button");
+    option.innerHTML = currentQuestion.option[i];
+    option.id = i;
+    option.className = "carts";
+    optionContainer.appendChild(option);
+    option.setAttribute("type", "button");
+    option.setAttribute("onclick", "getResult(this)");
+  }
+  questionCounter++;
 }
+
+
 
 //get the answer of current attempt question
-function getResult(element){
+function getResult(element) {
+  // timeCount();
+  const id = parseInt(element.id);
 
-    const id = parseInt(element.id);
-    // const id = element.id;
-    // console.log(typeof id);
-    //get the answer by compiring the id of clicked option
-    //|| id === currentQuestion.answer2 
-    if (id === currentQuestion.answer ){
-        //set the green color to the current option
-        element.classList.add("correct");
-        updateAnswerIndecator("correct");
+  //get the answer by compiring the id of clicked option
 
-        
-        correntAnswers ++; 
-        // console.log( "correct:"+ correntAnswers)
-    }else{
+  if (id === currentQuestion.answer) {
+    //set the green color to the current option
+    element.classList.add("correct");
+    updateAnswerIndecator("correct");
+    correntAnswers++;
+  } else {
+    //set the red color to inccorect options
+    element.classList.add("wrong");
+    updateAnswerIndecator("wrong");
+    wrongAnswers++;
 
-        //set the red color to inccorect options
-        element.classList.add("wrong")
-        updateAnswerIndecator("wrong");
-
-        //if the anser id 
-        const optionLen = optionContainer.children.length;
-        for(let i=0; i<optionLen; i++){
-            // || parseInt(optionContainer.children[i].id) === currentQuestion.answer2 
-    if(parseInt(optionContainer.children[i].id) === currentQuestion.answer  ){
-                optionContainer.children[i].classList.add("correct");
-                
-            }
-        }
-   
+    //if the anser id
+    const optionLen = optionContainer.children.length;
+    for (let i = 0; i < optionLen; i++) {
+      if (parseInt(optionContainer.children[i].id) === currentQuestion.answer) {
+        optionContainer.children[i].classList.add("correct");
+      }
     }
-    attempt++;
-    unclickableOption();
-    setTimeout(function(){
-        next();
-    }, 2000);
-    
-    
+  }
+  attempt++;
+  unclickableOption();
+  clearInterval(timer);
+  setTimeout(function () {
+    next();
+    timeCount();
+  }, 2000);
 }
 
-// make the buttons unclicble after the choice 
-function  unclickableOption(){
 
-    const optinLen = optionContainer.children.length;
 
-    for( let i=0 ;i<optinLen;i++ ){
+// make the buttons unclicble after the choice
+function unclickableOption() {
+  const optinLen = optionContainer.children.length;
 
-        optionContainer.children[i].classList.add("allready_answered");
-    }
-
-} 
-
-//ceat cercle indecators 
-function answerIndecator(){
-
-    const totalQuestion = quiz.length;
-
-    for(let i=0 ; i<totalQuestion ;i++){
-        const indicator = document.createElement("div");
-        answerIndecatorContainer.appendChild(indicator);
-    }
-   
+  for (let i = 0; i < optinLen; i++) {
+    optionContainer.children[i].classList.add("allready_answered");
+  }
 }
+
+
+
+//ceat cercle indecators
+function answerIndecator() {
+  const totalQuestion = quiz.length;
+
+  for (let i = 0; i < totalQuestion; i++) {
+    const indicator = document.createElement("div");
+    answerIndecatorContainer.appendChild(indicator);
+  }
+}
+
+
 
 //this function adds to the the button answers the style eather wrong or correct
-
- // marktype: correct or wrong 
-
-function updateAnswerIndecator(marktype){
-
-    answerIndecatorContainer.children[questionCounter-1].classList.add(marktype);
-
+// marktype: correct or wrong
+function updateAnswerIndecator(marktype) {
+  answerIndecatorContainer.children[questionCounter - 1].classList.add(
+    marktype
+  );
 }
+
+
 
 //functin to move to the next quistion
 function next(){
+  if (questionCounter === quiz.length) {
+    quizOver();
+  } else {
+    getNewQuestion();
+  }
+}
 
-    if (questionCounter === quiz.length){
-        quizOver();
-    }else{
-        getNewQuestion();
-        
-    }
+
+
+// i used this to show the result page
+function quizOver() {
+  //hide quiz page
+  quizBox.classList.add("hide");
+  //show quiz page
+  resultBox.classList.add("remove");
+  quizResult();
+   clearInterval(timer);
 
 }
 
-// i used this to show the result page 
-function quizOver(){
-    //hide quiz page
-    quizBox.classList.add("hide");
 
-    //show quiz page
-    resultBox.classList.add("remove");
-    quizResult();
-}
 
 //this function is responsible of showing the result statistics
-function quizResult(){
+function quizResult() {
+  //showing the stepper result
+  const element = document.getElementById("Resault");
+  element.setAttribute("class", "active");
+  // result
+  resultBox.querySelector(".total-score").innerHTML =
+    correntAnswers * 20 + " pts";
 
-    //showing the stepper result
-    const element = document.getElementById('Resault');
-         element.setAttribute('class', 'active');
+  const percentage = (correntAnswers / quiz.length) * 100;
 
-    // result
-    resultBox.querySelector(".total-quistion").innerHTML= quiz.length;
+  let per = (resultBox.querySelector(".total-percentage").innerHTML =
+    percentage.toFixed() + "%");
 
-    resultBox.querySelector(".total-score").innerHTML=correntAnswers*20+" pts";
+  d = parseInt(per);
 
-    const percentage = (correntAnswers/quiz.length)*100;
+  if (d < 50) {
+    resultBox.querySelector(".total-quistion").innerHTML =
+      "you need a bit more documentations";
+  } else if (d >= 90) {
+    resultBox.querySelector(".total-quistion").innerHTML = "exllent !!";
+  } else {
+    resultBox.querySelector(".total-quistion").innerHTML = "good !!";
+  }
 
-    let per=resultBox.querySelector(".total-percentage").innerHTML= percentage.toFixed()+"%";
+  resultBox.querySelector(".total-correct").innerHTML = correntAnswers;
+  resultBox.querySelector(".total-wrong").innerHTML = wrongAnswers;
 
-    if(per < 50){
+}
 
-    }else if(per<90){
 
-    }else{
 
+//counts the timming of eache quistion starting from 30s
+function timeCount() {
+  let sec = 5;
+
+  timer = setInterval(function () {
+    document.getElementById("timer").innerHTML = sec;
+    sec--;
+
+    if (sec == -1) {
+      updateAnswerIndecator("wrong");
+      wrongAnswers++;
+      clearInterval(timer);
+      next();
+      timeCount();
     }
+  }, 1000);
 
-    resultBox.querySelector(".total-correct").innerHTML=correntAnswers;
-
-    resultBox.querySelector(".total-wrong").innerHTML=attempt-correntAnswers;
-   
-}
-
-//counts the timming of eache quistion starting from 30s 
-
-// function timeCount(){
-   
-//         var sec = 3;
-//         const timer= setInterval(function() {
-//           document.getElementById("timer").innerHTML =  sec;
-//           sec--;
-    
-//             if (sec == -1) {
-                
-//                 updateAnswerIndecator("wrong");
-//                 next();
-//                 clearInterval(timer);
-               
-                
-//               }
-           
-              
-          
-//         }, 1000);
-// }
-
-// execute js fanctions 
-window.onload = function(){
-    
-    // timeCount(); 
-
-    setAvailableQuestions();
-
-    getNewQuestion();
-
-    answerIndecator();
-   
-   
 }
 
 
 
+// execute js fanctions
+window.onload = function () {
 
+  timeCount();
+  setAvailableQuestions();
+
+  getNewQuestion();
+
+  answerIndecator();
+
+};
